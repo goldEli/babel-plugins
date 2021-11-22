d30c3cf8e2acfe0bc950973c9839d09561810cab
miaoyu <miaoyu2009@qq.com>
2021-11-22 14:13:38 +0800

    ✨改造为babeljs插件

--- packages/plugin-console/src/index.js
+++ packages/plugin-console/src/index.js
@@ -1,56 +1,13 @@
-const parser = require("@babel/parser");
-const traverse = require("@babel/traverse").default;
-const generator = require("@babel/generator").default;
-const types = require("@babel/types");
-const template = require("@babel/template").default
+const { transformFileSync } = require('@babel/core');
+const insertParametersPlugin = require('./plugin/parameters-insert-plugin');
+const path = require('path');
 
-const sourceCode = `
-    console.log(1);
-
-    function func() {
-        console.info(2);
+const { code } = transformFileSync(path.join(__dirname, './sourceCode.js'), {
+    plugins: [insertParametersPlugin],
+    parserOpts: {
+        sourceType: 'unambiguous',
+        plugins: ['jsx']       
     }
-
-    export default class Clazz {
-        say() {
-            console.debug(3);
-        }
-        render() {
-            return <div>{console.error(4)}</div>
-        }
-    }
-`;
-
-const ast = parser.parse(sourceCode, {
-  sourceType: "unambiguous",
-  plugins: ["jsx"],
 });
-
-const targetCalleeName = ["log", "info", "error", "debug"].map(
-  (item) => `console.${item}`
-);
-
-traverse(ast, {
-  CallExpression(path, state) {
-		if (path.node.isNew) {
-			return;
-		}
-    const calleeName = generator(path.node.callee).code;
-    if (targetCalleeName.includes(calleeName)) {
-      const { line, column } = path.node.loc.start;
-			const newNode = template.expression(`console.log("filename: (${line}, ${column})")`)();
-			newNode.isNew = true;
-			if (path.findParent(path => path.isJSXElement())) {
-				path.replaceWith(types.arrayExpression([newNode, path.node]))
-				path.skip()
-			} else {
-				path.insertBefore(newNode)
-			}
-      
-    }
-  },
-});
-
-const { code } = generator(ast);
 
 console.log(code);
